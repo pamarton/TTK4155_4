e203fdc0a5b7774f94c56f79fc0772bb40b1e684
unknown <dirkpr@itk-d667.win.ntnu.no>
2017-10-02 13:41:11 +0200

    Implemented child-parent navigation and function call in the menu

--- 2509g17/TTK4155-master/Oving1_TTK4155_G17_H17/Oving1_TTK4155_G17_H17/menu.c
+++ 2509g17/TTK4155-master/Oving1_TTK4155_G17_H17/Oving1_TTK4155_G17_H17/menu.c
@@ -23,8 +23,8 @@ void setup_menu(menu * new_menu, menu * ptr_sib_down, menu * ptr_parent, menu * 
 	
 const char mainmenu_entry_0[16] PROGMEM = "Start";
 const char mainmenu_entry_1[16] PROGMEM = "highscore";
-const char mainmenu_entry_2[16] PROGMEM = "Options";
-const char mainmenu_entry_3[16] PROGMEM = "Debug";
+const char mainmenu_entry_2[16] PROGMEM = "Debug";
+const char mainmenu_entry_3[16] PROGMEM = "Options";
 const char highscore_entry_0[16] PROGMEM = "Puzzyzlayer69";
 const char highscore_entry_1[16] PROGMEM = "X_xMlGPr0_x_Y";
 const char highscore_entry_2[16] PROGMEM = "Dirk_tryhardt";
@@ -40,89 +40,48 @@ const char debug_entry_3[16] PROGMEM = "debug3";
 
 char temp[16];
 
-menu highscore_0;
-menu highscore_1;
-menu highscore_2;
+//menu highscore_0;
+//menu highscore_1;
+//menu highscore_2;
 menu option_0;
 menu option_1;
-menu option_2;
-menu option_3;
+//menu option_2;
+//menu option_3;
 menu debug_0;
 menu debug_1;
-menu debug_2;
-menu debug_3;
-menu mainmenu_0;
-menu mainmenu_1;//Highscore
-//menu mainmenu_2;
+//menu mainmenu_0;//Highscore
+menu mainmenu_1;
+menu mainmenu_2;
 //menu mainmenu_3;
 
 void initialize_menu(void){
-	setup_menu(&highscore_0,&highscore_1,&mainmenu_1,NULL,3,highscore_entry_0);
-	setup_menu(&highscore_1,&highscore_2,&mainmenu_1,NULL,3,highscore_entry_1);
-	setup_menu(&highscore_2,&highscore_0,&mainmenu_1,NULL,3,highscore_entry_2);
-	current_head = &highscore_0;
-	current_selected = &highscore_1;
-	//printf("%i\n",highscore_0.ptr_sib_down);
-	//printf("%i\n",current_head->ptr_self);
-	//strcpy_P(temp,(current_head->title));
-	//printf("%s",temp);
+	//setup_menu(&highscore_0,&highscore_1,&mainmenu_0,NULL,3,highscore_entry_0);
+	//setup_menu(&highscore_1,&highscore_2,&mainmenu_0,NULL,3,highscore_entry_1);
+	//setup_menu(&highscore_2,&highscore_0,&mainmenu_0,NULL,3,highscore_entry_2);
 	
-	// call highscore 1 through highscore 0
-	//strcpy_P(temp, current_head->title));
-	//printf("highscore1, called by highscore 0; title: %s\n",(highscore_0.ptr_sib_up),(highscore_0.ptr_sib_down), temp);
+	//setup_menu(&mainmenu_0,&mainmenu_1,NULL,&highscore_0,3,mainmenu_entry_1);
+	setup_menu(&mainmenu_1,&mainmenu_2,NULL,&debug_0,2,mainmenu_entry_2);
+	setup_menu(&mainmenu_2,&mainmenu_1,NULL,&option_0,2,mainmenu_entry_3);
 	
+	setup_menu(&option_0,&option_1,&mainmenu_1,NULL,2,option_entry_0);
+	setup_menu(&option_1,&option_0,&mainmenu_1,NULL,2,option_entry_1);
+	//setup_menu(&option_2,&option_3,&mainmenu_2,NULL,4,option_entry_2);
+	//setup_menu(&option_3,&option_0,&mainmenu_2,NULL,4,option_entry_3);
 	
-	
-
-	//strcpy_P(temp,highscore_0->title);
-	//printf("0self %i up %i down %i prev %i next %i sibl %i title %s\n",highscore_0->ptr_self,highscore_0->ptr_sib_up);
-	
-	//printf("self %c up %c down %c prev %c next %c sibl %c title %s\n",(*highscore_0.ptr_self),(*highscore_0.ptr_sib_up),(*highscore_0.ptr_sib_down),(*highscore_0.ptr_parent),(*highscore_0.ptr_child),(*highscore_0.n_sib),temp);
-	
-	//setup_menu(&highscore_1,&highscore_0,&highscore_2,NULL,NULL,3,highscore_n1);
-	//_delay_ms(1000);
-	//strcpy_P(temp,highscore_0->title);
-	//printf("0self %i up %i down %i prev %i next %i sibl %i title %s\n",(highscore_0.ptr_self),(highscore_0.ptr_sib_up),(highscore_0.ptr_sib_down),(highscore_0.ptr_parent),(highscore_0.ptr_child),(highscore_0.n_sib),temp);
+	setup_menu(&debug_0,&debug_1,&mainmenu_1,NULL,2,debug_entry_0);
+	setup_menu(&debug_1,&debug_0,&mainmenu_1,NULL,2,debug_entry_1);
 	
 	
-	//strcpy_P(temp,(char*)pgm_read_word(&highscore_0->title));
-	//printf("1self %i up %i down %i prev %i next %i sibl %i title %s\n",highscore_0->ptr_self,highscore_0->ptr_sib_up,highscore_0->ptr_sib_down,highscore_0->ptr_parent,highscore_0->ptr_child,highscore_0->n_sib,temp);
+	current_head = &mainmenu_1;
+	current_selected = &mainmenu_1;
 	
-	
-	//strcpy_P(temp,highscore_1->title);
-	//printf("self %i up %i down %i prev %i next %i sibl %i title %s\n",&highscore_1->ptr_self,&highscore_1->ptr_sib_up,&highscore_1->ptr_sib_down,&highscore_1->ptr_parent,&highscore_1->ptr_child,highscore_1->n_sib,temp);
-	
-	
-	/*setup_menu(highscore_2,highscore_1,highscore_0,mainmenu_1,NULL,3,highscore_n2);
-	
-	setup_menu(option_0,option_3,option_1,mainmenu_2,NULL,4,option_n0);
-	setup_menu(option_1,option_0,option_2,mainmenu_2,NULL,4,option_n1);
-	setup_menu(option_2,option_1,option_3,mainmenu_2,NULL,4,option_n2);
-	setup_menu(option_3,option_2,option_0,mainmenu_2,NULL,4,option_n3);
-	
-	setup_menu(debug_0,debug_3,debug_1,mainmenu_3,NULL,4,option_n0);
-	setup_menu(debug_1,debug_0,debug_2,mainmenu_3,NULL,4,option_n1);
-	setup_menu(debug_2,debug_1,debug_3,mainmenu_3,NULL,4,option_n2);
-	setup_menu(debug_3,debug_2,debug_0,mainmenu_3,NULL,4,option_n3);*/
-	
-	//setup_menu(mainmenu_0,mainmenu_3,mainmenu_1,NULL,NULL/*BYTT*/,4,mainmenu_n0);
-	//setup_menu(mainmenu_1,mainmenu_0,mainmenu_2,NULL,highscore_0,4,mainmenu_n1);
-	//setup_menu(mainmenu_2,mainmenu_1,mainmenu_3,NULL,option_0,4,mainmenu_n2);
-	//setup_menu(mainmenu_3,mainmenu_2,mainmenu_0,NULL,debug_0,4,mainmenu_n3);
-	//printf("%i",*current_head->ptr_self);
-	
-	/*
-	current_head = highscore_0;
-	strcpy_P(temp,current_head->title);
-	printf("%s ",temp);
-	current_selected = highscore_0;
-	
-	printf("%i",*current_selected);*/
 }
 	
 
 void menu_update(void){
-	if(navigate_menu()==1){
+	if(navigate_menu()){
+		oled_reset();
+		//printf("\n%i\n",current_head->n_sib);
 		for (int i = 0; i < current_head->n_sib; i++)
 		{
 			oled_goto_line(i);
@@ -137,12 +96,7 @@ void menu_update(void){
 			current_head = current_head->ptr_sib_down;
 			
 		}
-	}
-	
-	
-	
-	
-	
+	}	
 }
 
 int x = 0;
@@ -150,18 +104,49 @@ int y = 0;
 #define NAVIGATION_TRHESHOLD 50
 int navigate_menu(void){
 	int joystick_reading = read_control_input('Y');
-	if(joystick_reading > NAVIGATION_TRHESHOLD && x <= NAVIGATION_TRHESHOLD){
+	if(joystick_reading > NAVIGATION_TRHESHOLD && y <= NAVIGATION_TRHESHOLD){
 		for(int i = 0; i < current_selected->n_sib-1; i++){
 			current_selected = current_selected->ptr_sib_down;
 		}
-		x = 100;
+		y = 100;
 		return 1;
-	}else if (joystick_reading < -NAVIGATION_TRHESHOLD && x >= -NAVIGATION_TRHESHOLD)
+	}else if (joystick_reading < -NAVIGATION_TRHESHOLD && y >= -NAVIGATION_TRHESHOLD)
 	{
 		current_selected = current_selected->ptr_sib_down;
-		x = -100;
+		y = -100;
 		return 1;
+	}else if(joystick_reading < NAVIGATION_TRHESHOLD && joystick_reading > -NAVIGATION_TRHESHOLD){
+		y = 0;
+	}
+	
+	
+	joystick_reading = read_control_input('X');
+	if(joystick_reading > NAVIGATION_TRHESHOLD && x <= NAVIGATION_TRHESHOLD){
+		if(current_selected->ptr_child != NULL){
+			current_head = current_selected->ptr_child;
+			current_selected = current_selected->ptr_child;
+		}else{
+			strcpy_P(temp,current_selected->title);
+			printf("running function %s\n",temp);
+		}
+		x = 100;
+		return 1;
+		
+	}else if (joystick_reading < -NAVIGATION_TRHESHOLD && x >= -NAVIGATION_TRHESHOLD)
+	{
+		if(current_selected->ptr_parent != NULL){
+			current_head = current_selected->ptr_parent;
+			current_selected = current_selected->ptr_parent;
+			x = -100;
+			return 1;
+		}
 	}else if(joystick_reading < NAVIGATION_TRHESHOLD && joystick_reading > -NAVIGATION_TRHESHOLD){
 		x = 0;
 	}
+	
+	
+	
+	
+	
+	return 0;
 }
